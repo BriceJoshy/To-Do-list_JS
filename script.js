14a76143aaeb9e6418337d0bf0cf3b6c540c9b33
BRICE JOSHY <bricejoshy99@gmail.com>
2024-03-18 18:20:12 +0530

    resetting the textbox

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ let to_do_list = [];
 
 function add_task_1() {
   const task_1 = document.querySelector(".input_1");
-  let task = task_1.value;
+  const task = task_1.value;
   to_do_list.push(task);
   console.log(to_do_list);
+
+  //   resetting the textbox
+  task_1.value = "";
 }
